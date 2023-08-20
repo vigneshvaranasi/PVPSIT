f4fadecb9f0595fcae7c4a734209c256fc3e631f
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2023-08-20 16:08:44 +0530

    Update Js For cards

--- Clubs/home.js
+++ Clubs/home.js
@@ -1,28 +1,105 @@
+
+/* Explore Now JS Start */
+
 function smoothScroll(target) {
     const targetElement = document.querySelector(target);
-    if (targetElement) {
+    if (targetElement) 
+    {
         const targetPosition = targetElement.offsetTop;
         const startPosition = window.pageYOffset;
         const distance = targetPosition - startPosition;
         const duration = 800; 
         let start = null;
-
-        function step(timestamp) {
-            if (!start) start = timestamp;
-            const progress = timestamp - start;
-            window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-            if (progress < duration) {
-                window.requestAnimationFrame(step);
-            }
+        
+        function step(timestamp) 
+        {
+          if (!start) start = timestamp;
+          const progress = timestamp - start;
+          window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+          if (progress < duration) {
+            window.requestAnimationFrame(step);
+          }
         }
-
+        
         function easeInOutCubic(t, b, c, d) {
-            t /= d / 2;
+          t /= d / 2;
             if (t < 1) return c / 2 * t * t * t + b;
             t -= 2;
             return c / 2 * (t * t * t + 2) + b;
-        }
+          }
 
         window.requestAnimationFrame(step);
     }
 }
+
+/* Explore Now JS End */
+
+
+
+/* Innovatoion Cards automatic Start */
+
+  var myIndex = 0;
+  carousel();
+
+  function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 10000); // Change image every 2 seconds
+}
+
+/* Innovatoion Cards automatic Start */
+
+
+/* Innovation Cards Arrows */
+
+var slideIndex = 1;
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("slide");
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    } else if (n < 1) {
+        slideIndex = slides.length;
+    }
+    
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+}
+
+
+function pauseSlideshow() {
+    clearTimeout(slideshowTimer);
+}
+
+
+function resumeSlideshow() {
+    slideshowTimer = setTimeout(carousel, 2000);
+}
+
+document.querySelector(".prev").addEventListener("click", function () {
+    plusSlides(-1);
+    pauseSlideshow();
+});
+
+document.querySelector(".next").addEventListener("click", function () {
+    plusSlides(1);
+    pauseSlideshow();
+});
+
+
+var slideshowTimer;
+carousel();
