f445d181c33ebc1b64d0cacd5103c30d8bf7a55f
Vignesh Varanasi <121240801+vigneshvaranasi@users.noreply.github.com>
2023-09-30 15:38:14 +0530

    Update home.js

--- Clubs/home.js
+++ Clubs/home.js
@@ -2,111 +2,130 @@
 /* Explore Now JS Start */
 
 function smoothScrolls(target) {
-    const targetElement = document.querySelector(target);
-    if (targetElement) 
-    {
-        const targetPosition = targetElement.offsetTop;
-        const startPosition = window.pageYOffset;
-        const distance = targetPosition - startPosition;
-        const duration = 800; 
-        let start = null;
-        
-        function step(timestamp) 
-        {
-          if (!start) start = timestamp;
-          const progress = timestamp - start;
-          window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-          if (progress < duration) {
-            window.requestAnimationFrame(step);
-          }
+  const targetElement = document.querySelector(target);
+  if (targetElement) 
+  {
+      const targetPosition = targetElement.offsetTop;
+      const startPosition = window.pageYOffset;
+      const distance = targetPosition - startPosition;
+      const duration = 800; 
+      let start = null;
+      
+      function step(timestamp) 
+      {
+        if (!start) start = timestamp;
+        const progress = timestamp - start;
+        window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+        if (progress < duration) {
+          window.requestAnimationFrame(step);
         }
-        
-        function easeInOutCubic(t, b, c, d) {
-          t /= d / 2;
-            if (t < 1) return c / 2 * t * t * t + b;
-            t -= 2;
-            return c / 2 * (t * t * t + 2) + b;
-          }
+      }
+      
+      function easeInOutCubic(t, b, c, d) {
+        t /= d / 2;
+          if (t < 1) return c / 2 * t * t * t + b;
+          t -= 2;
+          return c / 2 * (t * t * t + 2) + b;
+        }
 
-        window.requestAnimationFrame(step);
-    }
+      window.requestAnimationFrame(step);
+  }
 }
 
 /* Explore Now JS End */
 
 /* Menu in Phone View Start */
 function toggleMenu() {
-  var sideMenu = document.querySelector('.side-menu');
-  sideMenu.classList.toggle('menu-open');
+var sideMenu = document.querySelector('.side-menu');
+sideMenu.classList.toggle('menu-open');
 }
 /* Menu in Phone View end */
 
 /* About Code Start */
 
 function smoothScroll(target) {
-  var targetElement = document.querySelector(target);
-  if (targetElement) {
-      window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: 'smooth'
-      });
-  }
+var targetElement = document.querySelector(target);
+if (targetElement) {
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
+    });
+}
 }
 
 document.querySelector('.about').addEventListener('click', function (event) {
-  event.preventDefault();
-  smoothScroll(this.getAttribute('href'));
+event.preventDefault();
+smoothScroll(this.getAttribute('href'));
 });
 
 /* About Code End */
-// Create a list of slides
-const slides = document.querySelectorAll(".slide");
 
-// Set the current slide index
-let currentSlideIndex = 0;
 
-// Add event listeners to the buttons
-document.querySelector(".prev").addEventListener("click", function() {
-  showSlide(currentSlideIndex - 1);
-});
 
-document.querySelector(".next").addEventListener("click", function() {
-  showSlide(currentSlideIndex + 1);
-});
+/* Innovatoion Cards automatic Start */
 
-function showSlide(index) {
-  // Check if the index is within bounds
-  if (index < 0) {
-    currentSlideIndex = slides.length - 1;
-  } else if (index >= slides.length) {
-    currentSlideIndex = 0;
-  } else {
-    currentSlideIndex = index;
-  }
+var myIndex = 0;
+carousel();
 
-  // Show or hide the arrows based on the current slide index
-  const prevButton = document.querySelector(".prev");
-  const nextButton = document.querySelector(".next");
-  if (currentSlideIndex === 0) {
-    prevButton.style.display = "none";
-  } else {
-    prevButton.style.display = "block";
-  }
-  if (currentSlideIndex === slides.length - 1) {
-    nextButton.style.display = "none";
-  } else {
-    nextButton.style.display = "block";
-  }
-
-  // Scroll to the slide
-  slides[currentSlideIndex].scrollIntoView({
-    behavior: "smooth",
-    block: "nearest",
-    inline: "start"
-  });
+function carousel() {
+var i;
+var x = document.getElementsByClassName("slide");
+for (i = 0; i < x.length; i++) {
+  x[i].style.display = "none";  
+}
+myIndex++;
+if (myIndex > x.length) {myIndex = 1}    
+x[myIndex-1].style.display = "block";  
+setTimeout(carousel, 6000); 
 }
 
-// Start the carousel
-setInterval(function() {
-  showSlide(currentSlideIndex + 1);
-}, 5000);
+/* Innovatoion Cards automatic Start */
+
+
+/* Innovation Cards Arrows */
+
+var slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var slides = document.getElementsByClassName("slide");
+  
+  if (n > slides.length) {
+      slideIndex = 1;
+  } else if (n < 1) {
+      slideIndex = slides.length;
+  }
+  
+  for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex - 1].style.display = "block";
+}
+
+
+function pauseSlideshow() {
+  clearTimeout(slideshowTimer);
+}
+
+
+function resumeSlideshow() {
+  slideshowTimer = setTimeout(carousel, 2000);
+}
+
+document.querySelector(".prev").addEventListener("click", function () {
+  plusSlides(-1);
+  pauseSlideshow();
+});
+
+document.querySelector(".next").addEventListener("click", function () {
+  plusSlides(1);
+  pauseSlideshow();
+});
+
+
+var slideshowTimer;
+carousel();
